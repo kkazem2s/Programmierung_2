be390df195b83e0007be652ec21853726377ff9d
kkazem2s <kevin.kazemali@smail.inf.h-brs.de>
2021-05-17 17:50:22 +0200

    todo

--- src/uebungsaufgaben/uebung04/DynArrayTest.java
+++ src/uebungsaufgaben/uebung04/DynArrayTest.java
@@ -5,14 +5,7 @@ import org.junit.jupiter.api.Test;
 public class DynArrayTest {
 
     @Test
-    public void createDynArray() {
-        DynArray<Integer> dynArray = new DynArray<>();
-
-        dynArray.addFirst(1);
-        System.out.println(dynArray);
-        dynArray.addFirst(2);
-        System.out.println(dynArray);
-        dynArray.addLast(3);
-        System.out.println(dynArray);
+    public void test() {
+        // TODO
     }
 }
