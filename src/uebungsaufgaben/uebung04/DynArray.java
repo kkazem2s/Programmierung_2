10807544f26de48ff973d38ccc7b2e4cc7ceb327
kkazem2s <kevin.kazemali@smail.inf.h-brs.de>
2021-05-17 17:50:08 +0200

    fix

--- src/uebungsaufgaben/uebung04/DynArray.java
+++ src/uebungsaufgaben/uebung04/DynArray.java
@@ -1,29 +1,77 @@
 package uebungsaufgaben.uebung04;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unchecked")
 public class DynArray<T> {
     private T[] dynArray;
     private int size;
 
     public DynArray() {
+        size = 0;
         dynArray = (T[]) new Object[1];
-        size = 1;
     }
-    public int capacity() {
-        return dynArray.length;
+
+    public void addFirst(T e) {
+        check();
+        T[] dynArray = (T[]) new Object[this.dynArray.length];
+        for (int i = 1; i < dynArray.length; i++) {
+            dynArray[i] = this.dynArray[i-1];
+        }
+        dynArray[0] = e;
+        size++;
+        this.dynArray = dynArray;
     }
-    public int size() {
-        return size;
+
+    public void addLast(T e) {
+        check();
+        this.dynArray[size-1] = e;
+        size++;
     }
+
+    public T removeFirst() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Cannot remove from Empty Array");
+        } else {
+            T[] dynArray = (T[]) new Object[this.dynArray.length];
+            T tmp = this.dynArray[0];
+            for(int i = 1; i < dynArray.length; i++) {
+                dynArray[i-1] = this.dynArray[i];
+            }
+            this.dynArray = dynArray;
+            size--;
+            check();
+            return tmp;
+        }
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Cannot remove from Empty Array");
+        } else {
+            T[] dynArray = (T[]) new Object[this.dynArray.length];
+            T tmp = this.dynArray[this.dynArray.length-1];
+            for(int i = 0; i < this.dynArray.length-1; i++) {
+                dynArray[i] = this.dynArray[i];
+            }
+            this.dynArray = dynArray;
+            size--;
+            check();
+            return tmp;
+        }
+    }
+
     public T get(int pos) {
-        if (!(pos > 0 && pos < dynArray.length)) {
+        pos -= 1;
+        if (pos < 0 || pos >= size-1) {
             throw new IndexOutOfBoundsException();
         } else {
             return dynArray[pos];
         }
     }
+
     public T set(int pos, T e) {
-        if (!(pos > 0 && pos < dynArray.length)) {
+        if (pos < 0 || pos >= size-1) {
             throw new IndexOutOfBoundsException();
         } else {
             T tmp = dynArray[pos];
@@ -33,85 +81,41 @@ public class DynArray<T> {
         }
     }
 
-    public void addFirst(T e) {
-        if (size == 1) {
-            dynArray[0] = e;
-            check();
-        } else {
-            check();
-            T[] dynArray = (T[]) new Object[this.dynArray.length];
-            for (int i = 1; i < dynArray.length; i++) {
-                dynArray[i] = this.dynArray[i-1];
-            }
-            dynArray[0] = e;
-            size++;
-            this.dynArray = dynArray;
-        }
-    }
-
-    public void addLast(T e) {
-        check();
-        T[] dynArray = (T[]) new Object[this.dynArray.length];
-        dynArray[size] = e;
-        size++;
-        this.dynArray = dynArray;
-    }
-
-    public T removeFirst() {
-        T[] dynArray = (T[]) new Object[this.dynArray.length];
-        T tmp = this.dynArray[0];
-        for(int i = 1; i < dynArray.length; i++) {
-            dynArray[i] = this.dynArray[i];
-        }
-        this.dynArray = dynArray;
-        size--;
-        check();
-        return tmp;
-    }
-
-    public T removeLast() {
-        T[] dynArray = (T[]) new Object[this.dynArray.length];
-        T tmp = this.dynArray[this.dynArray.length-1];
-        for(int i = 0; i < this.dynArray.length-1; i++) {
-            dynArray[i] = this.dynArray[i];
-        }
-        this.dynArray = dynArray;
-        size--;
-        check();
-        return tmp;
-    }
-
     private void check() {
-        if (size == dynArray.length-1) {
+        if (size == dynArray.length) {
             doubleSize();
         } else {
-            if (dynArray.length*4 <= size) {
+            if ((size-1)*4 == dynArray.length || size == 1) {
                 halfSize();
             }
         }
     }
-
     private void doubleSize() {
         T[] dynArray = (T[]) new Object[this.dynArray.length*2];
         for (int i = 0; i < this.dynArray.length; i++) {
-            dynArray[i] = dynArray[i];
+            dynArray[i] = this.dynArray[i];
+        }
+        this.dynArray = dynArray;
+    }
+    private void halfSize() {
+        T[] dynArray = (T[]) new Object[this.dynArray.length/2];
+        for (int i = 0; i < dynArray.length; i++) {
+            dynArray[i] = this.dynArray[i];
         }
         this.dynArray = dynArray;
     }
 
-    private void halfSize() {
-        T[] dynArray = (T[]) new Object[this.dynArray.length/2];
-        for (int i = 0; i < this.dynArray.length; i++) {
-            dynArray[i] = dynArray[i];
-        }
-        this.dynArray = dynArray;
+    public int capacity() {
+        return dynArray.length;
     }
+    public int size() {
+        return size;
+    }
+    public int getL() {
+        return dynArray.length;
+    }
+
     public String toString() {
-        String erg = "[";
-        for(int i = 0; i < dynArray.length-1; i++) {
-            erg += dynArray[i].toString() + ",";
-        }
-        erg += dynArray[dynArray.length-1].toString() + "]";
-        return erg;
+        return Arrays.toString(this.dynArray);
     }
 }
