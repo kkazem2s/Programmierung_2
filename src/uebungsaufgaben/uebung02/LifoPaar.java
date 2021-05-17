b5c459d40c44cb994462b3a7b43ec1d392c6144b
kkazem2s <kevin.kazemali@smail.inf.h-brs.de>
2021-05-17 19:09:39 +0200

    added

--- src/uebungsaufgaben/uebung02/LifoPaar.java
+++ src/uebungsaufgaben/uebung02/LifoPaar.java
@@ -1,5 +1,19 @@
 package uebungsaufgaben.uebung02;
 
+import uebungsaufgaben.uebung00.Lifo;
+
 public class LifoPaar {
-    // TODO
+    public static void main(String [] args) {
+        Lifo<Paar<Integer, String>> paarLifo = new Lifo<>();
+
+        paarLifo.push(new Paar<>(4, "Goodbye"));
+        paarLifo.push(new Paar<>(3, "!"));
+        paarLifo.push(new Paar<>(2, "World"));
+        paarLifo.push(new Paar<>(1, "Hello"));
+
+        System.out.println(paarLifo.pop());
+        System.out.println(paarLifo.pop());
+        System.out.println(paarLifo.pop());
+        System.out.println(paarLifo.pop());
+    }
 }
