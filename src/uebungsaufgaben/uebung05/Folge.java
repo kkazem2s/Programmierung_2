abe77027e17b614ff8300c5d0a55beecd9ea5d8e
kkazem2s <kevin.kazemali@smail.inf.h-brs.de>
2021-05-18 17:01:30 +0200

    update interfaces

--- src/uebungsaufgaben/uebung05/Folge.java
+++ src/uebungsaufgaben/uebung05/Folge.java
@@ -7,7 +7,7 @@ public interface Folge<T> extends Puffer<T> {
     void set(int pos, T e) throws IndexOutOfBoundsException, NoSuchElementException;
     T get(int index) throws IndexOutOfBoundsException;
     @Override
-    void insert (T e) throws UnsupportedOperationException, NoSuchElementException;
+    void insert (T e) throws UnsupportedOperationException;
     @Override
-    T remove() throws UnsupportedOperationException, NoSuchElementException;
+    T remove() throws UnsupportedOperationException;
 }
--- src/uebungsaufgaben/uebung05/Stack.java
+++ src/uebungsaufgaben/uebung05/Stack.java
@@ -1,12 +1,5 @@
 package uebungsaufgaben.uebung05;
 
-import java.util.EmptyStackException;
-
 public interface Stack<T> extends Puffer<T> {
-
     T top();
-    @Override
-    T remove() throws EmptyStackException;
-    @Override
-    void insert(T e);
 }
