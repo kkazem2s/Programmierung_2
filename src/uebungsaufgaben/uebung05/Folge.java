394019a2cb4d284b9ff1eefe32ddc01dc3b071c2
kkazem2s <kevin.kazemali@smail.inf.h-brs.de>
2021-05-19 17:49:25 +0200

    updated?

--- src/uebungsaufgaben/uebung05/Folge.java
+++ src/uebungsaufgaben/uebung05/Folge.java
@@ -1,7 +1,6 @@
 package uebungsaufgaben.uebung05;
 
 public interface Folge<T> extends Puffer<T> {
-
     void set(int pos, T e) throws IndexOutOfBoundsException;
     T get(int index) throws IndexOutOfBoundsException;
     @Override
